d9b44f3c7afb645e67c652f19bfe34d6e25855c7
Conor Fleming <32988273+Conor-Fleming@users.noreply.github.com>
2022-07-14 22:26:49 -0700

    starting project, trying to build http server. Need to test/Understand

--- SocialMediaBE/main.go
+++ SocialMediaBE/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+		"fmt"
+		"net/http"
+		"log"
+		"time"
+)
 
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", testHandler)
+
+	const addr = "localhost:8080"
+
+	srv := http.Server {
+		Handler: mux,
+		Addr: addr,
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout: 30 * time.Second,
+	}
+
+	fmt.Println(addr)
+	err := srv.ListenAndServe()
+	log.Fatal(err)
+}
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write([]byte("{}"))
 }
