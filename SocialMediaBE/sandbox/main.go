bdbbf4717987126fd43d3b46b223936db2a0f86d
Conor Fleming <32988273+Conor-Fleming@users.noreply.github.com>
2022-07-23 19:34:14 -0700

    used to test database functionality

--- SocialMediaBE/sandbox/main.go
+++ SocialMediaBE/sandbox/main.go
@@ -13,5 +13,34 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("database ensured!")
+
+	user, err := c.CreateUser("test@example.com", "password", "john doe", 18)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("user created", user)
+
+	updatedUser, err := c.UpdateUser("test@example.com", "password", "john doe", 18)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("user updated", updatedUser)
+
+	gotUser, err := c.GetUser("test@example.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("user got", gotUser)
+
+	err = c.DeleteUser("test@example.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("user deleted")
+
+	_, err = c.GetUser("test@example.com")
+	if err == nil {
+		log.Fatal("shouldn't be able to get user that was deleted")
+	}
+	fmt.Println("user confirmed deleted")
 }
