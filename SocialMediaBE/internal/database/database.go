a2e5ef953e7c793a4259c3f7ed029fd2df03dc2d
Conor Fleming <32988273+Conor-Fleming@users.noreply.github.com>
2022-07-24 19:22:43 -0700

    creating seperate files for User and Post update functions

--- SocialMediaBE/internal/database/database.go
+++ SocialMediaBE/internal/database/database.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"os"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type User struct {
@@ -144,4 +146,35 @@ func NewClient(path string) Client {
 		filepath: path,
 	}
 	return newCli
+}
+
+func (c Client) CreatePost(userEmail, text string) (Post, error) {
+	schema, _ := c.readDB()
+	if _, ok := schema.Users[userEmail]; ok {
+		id := uuid.New().String()
+		newPost := Post{
+			ID:        id,
+			CreatedAt: time.Now().UTC(),
+			UserEmail: userEmail,
+			Text:      text,
+		}
+		schema.Posts[id] = newPost
+		c.updateDB(schema)
+		return newPost, nil
+	}
+	return Post{}, errors.New("There was an error creating the post")
+}
+
+func (c Client) GetPosts(userEmail string) ([]Post, error) {
+
+}
+
+func (c Client) DeletePost(id string) error {
+	schema, _ := c.readDB()
+	if _, ok := schema.Posts[id]; ok {
+		delete(schema.Posts, id)
+		c.updateDB(schema)
+		return nil
+	}
+	return errors.New("Error when deleting, ID doesnt exist")
 }
