e95327ff37e4ebee5d8089c78f588261df763798
Conor Fleming <32988273+Conor-Fleming@users.noreply.github.com>
2022-07-27 15:38:06 -0700

    setting up error handling and http response helpers

--- SocialMediaBE/main.go
+++ SocialMediaBE/main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/Conor-Fleming/SocialMediaBE/internal/database"
 )
+
+type errorBody struct {
+	Error string `json: "error"`
+}
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", testHandler)
+	mux.HandleFunc("/err", testErrorHandler)
 
 	const addr = "localhost:8080"
 
@@ -26,7 +35,36 @@ func main() {
 }
 
 func testHandler(w http.ResponseWriter, r *http.Request) {
+	//w.Header().Set("Content-Type", "application/json")
+	//w.WriteHeader(200)
+	//w.Write([]byte("{}"))
+	respondWithJSON(w, 200, database.User{
+		Email: "test@example.com",
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	w.Write([]byte("{}"))
+	response, err := json.Marshal(payload)
+	if err != nil {
+		log.Println("error marshalling", err)
+		w.WriteHeader(500)
+		response, _ := json.Marshal(errorBody{
+			Error: "error marshalling",
+		})
+		w.Write(response)
+		return
+	}
+	w.Write(response)
+	w.WriteHeader(code)
+}
+
+func testErrorHandler(w http.ResponseWriter, r *http.Request) {
+	respondWithError(w, 500, errors.New("server error"))
+}
+
+func respondWithError(w http.ResponseWriter, code int, err error) {
+	respondWithJSON(w, code, errorBody{
+		Error: err.Error(),
+	})
 }
