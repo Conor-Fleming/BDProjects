67ed8afa444d3e261f332c89d8a736397b3c0bea
Conor Fleming <32988273+Conor-Fleming@users.noreply.github.com>
2022-07-30 17:04:14 -0700

    created initial endpoint method, with create user handler func

--- SocialMediaBE/main.go
+++ SocialMediaBE/main.go
@@ -22,13 +22,19 @@ type apiConfig struct {
 func main() {
 	mux := http.NewServeMux()
 	dbClient := database.NewClient("db.json")
+	err := dbClient.EnsureDB()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	apiCfig := apiConfig{
 		dbClient: dbClient,
 	}
 
-	mux.HandleFunc("/", testHandler)
-	mux.HandleFunc("/err", testErrorHandler)
+	mux.HandleFunc("/users", apiCfig.endpointUsersHandler)
+	mux.HandleFunc("/users/", apiCfig.endpointUsersHandler)
+	//mux.HandleFunc("/", testHandler)
+	//mux.HandleFunc("/err", testErrorHandler)
 
 	const addr = "localhost:8080"
 
@@ -40,10 +46,49 @@ func main() {
 	}
 
 	fmt.Println(addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	log.Fatal(err)
 }
 
+func (apiCfig apiConfig) endpointUsersHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		// call GET handler
+	case http.MethodPost:
+		// call POST handler
+		apiCfig.handlerCreateUser(w, r)
+	case http.MethodPut:
+		// call PUT handler
+	case http.MethodDelete:
+		// call DELETE handler
+	default:
+		respondWithError(w, 404, errors.New("method not supported"))
+	}
+}
+
+func (apiCfig apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+		Name     string `json:"name"`
+		Age      int    `json:"age"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err)
+		return
+	}
+	result, err := apiCfig.dbClient.CreateUser(params.Email, params.Password, params.Name, params.Age)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err)
+		return
+	}
+	respondWithJSON(w, http.StatusCreated, result)
+}
+
+/*
 func testHandler(w http.ResponseWriter, r *http.Request) {
 	//w.Header().Set("Content-Type", "application/json")
 	//w.WriteHeader(200)
@@ -51,7 +96,7 @@ func testHandler(w http.ResponseWriter, r *http.Request) {
 	respondWithJSON(w, 200, database.User{
 		Email: "test@example.com",
 	})
-}
+}*/
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
@@ -69,9 +114,10 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.WriteHeader(code)
 }
 
+/*
 func testErrorHandler(w http.ResponseWriter, r *http.Request) {
 	respondWithError(w, 500, errors.New("server error"))
-}
+}*/
 
 func respondWithError(w http.ResponseWriter, code int, err error) {
 	respondWithJSON(w, code, errorBody{
